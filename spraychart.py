6b160e4e2cac9829c61c6627ae0be03bb0d71f7d
rj7002 <160825414+rj7002@users.noreply.github.com>
2024-08-25 23:07:28 -0700

    Update spraychart.py

--- spraychart.py
+++ spraychart.py
@@ -639,10 +639,10 @@ fig.update_layout(
 )
 
 # st.subheader(f'{hitters} Hits Chart')
-col1, col2 = st.columns(2)
 if filterby != 'Type Hit':
         for id in reversed(ids):
             display_player_image(id,250,'')
+col1, col2 = st.columns(2)
 with col1:
     st.plotly_chart(fig,use_container_width=True)
 
