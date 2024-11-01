3dea054e8d68c2e121fabc4a577e7860018cf182
rj7002 <160825414+rj7002@users.noreply.github.com>
2024-10-31 23:03:13 -0700

    Update spraychart.py

--- spraychart.py
+++ spraychart.py
@@ -264,7 +264,7 @@ if formatted_date:
         # st.write(df.columns)
         # st.write(df)
         hometeam = df['home_team'].iloc[0].lower()
-        stl_file_path = f'/Users/ryan/Downloads/ImageToStl.com_a97cc114677c453abd44ba241b899b11/stadium_{hometeam}.stl'
+        stl_file_path = f'stadium_{hometeam}.stl'
 
 
         scatter_points = df[['hc_x', 'hc_y']]  # Make sure these columns exist in your DataFrame
