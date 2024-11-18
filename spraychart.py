9d8691d8c9d9f94a3724ce33dcb8f3caf80b817c
Ryan Joseph <160825414+rj7002@users.noreply.github.com>
2024-11-17 17:17:15 -0800

    Update spraychart.py

--- spraychart.py
+++ spraychart.py
@@ -502,7 +502,7 @@ if formatted_date:
             ]
             
             # Create a figure for the legend with transparent background
-            legendfig, ax = plt.subplots(figsize=(3, 1.5), facecolor='none')  # 'none' ensures transparency
+            legendfig, ax = plt.subplots(figsize=(5, 3), facecolor='none')  # 'none' ensures transparency
             ax.axis('off')  # Turn off the axes
             
             # Add the legend to the figure
